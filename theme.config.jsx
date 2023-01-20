9eefc220c2d410bc52998cd65976ebf1fead11c5
SleepNov4 <frostgamers.fx@gmail.com>
2023-01-20 19:34:46 +0700

    Dev Patch #13

--- theme.config.jsx
+++ theme.config.jsx
@@ -3,6 +3,7 @@ import logo from './styles/style.js'
 import React from 'react'
 import { useRouter } from 'next/router'
 import { useConfig } from 'nextra-theme-docs'
+import Script from 'next/script'
 
 // eslint-disable-next-line import/no-anonymous-default-export
 export default {
@@ -19,6 +20,7 @@ export default {
         <meta property="og:description" content={frontMatter.description || 'A small community based on the internet dedicating for gaming and learning in programming & software developing.'} />
         <title>Hyperion Foundation</title>
         <link rel="icon" href="/hyperion-favicon.png" type="image/png" />
+        <Script async="true" src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-6163526907517542" crossOrigin="anonymous" />
       </>
     },
     i18n: [
@@ -28,7 +30,7 @@ export default {
     banner: {
       dismissible: false,
       key: 'Testing Purposes',
-      text: '👨‍🔧 This website is hosted for testing purposes only 👨‍🔧'
+      text: '👨‍🔧 This website version is hosted for testing purposes only 👨‍🔧'
     },
     search: {
       component: null
