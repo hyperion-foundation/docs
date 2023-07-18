ab9098ffde04cb7967f9175aee2b4e66e3a0032b
Alexander Andhika Suryaperdana <frostgamers.fx@gmail.com>
2023-07-18 18:03:38 +0700

    refactor '_app.js' to '_app.mdx'

--- pages/_app.js
+++
@@ -1,8 +0,0 @@
-import '../src/css/globals.css'
-// import Script from 'next/script'
-
-function MyApp({ Component, pageProps }) {
-    return <Component {...pageProps} />
-}
-
-export default MyApp
