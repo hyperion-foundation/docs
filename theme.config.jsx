2f10e677e840d119620a8f252b2751ceecb43a0c
Alexander Andhika S <frostgamers.fx@gmail.com>
2023-11-09 13:58:59 +0700

    test: added Google AdSense Ad Blocking recovery tag for testing

--- theme.config.jsx
+++ theme.config.jsx
@@ -24,21 +24,41 @@ export default {
       const { frontMatter } = useConfig()
       return (
         <>
-          <meta name="viewport" content="width=device-width, initial-scale=1" />
-          <meta name="og:url" content={`https://www.hyperfoundation.xyz${asPath}`} />
-          <meta name="og:title" content={frontMatter.title || 'Hyperion Foundation'} />
-          <meta name="og:description" content={frontMatter.description || 'A small community based on the internet dedicating for gaming and learning in programming & software developing.'} />
-          <meta name="description" content={frontMatter.description || 'A small community based on the internet dedicating for gaming and learning in programming & software developing.'} />
-          <meta name="google-adsense-account" content="ca-pub-6163526907517542" />
+          <meta name='viewport' content='width=device-width, initial-scale=1' />
+          <meta name='og:url' content={`https://www.hyperfoundation.xyz${asPath}`} />
+          <meta name='og:title' content={frontMatter.title || 'Hyperion Foundation'} />
+          <meta name='og:description' content={frontMatter.description || 'A small community based on the internet dedicating for gaming and learning in programming & software developing.'} />
+          <meta name='description' content={frontMatter.description || 'A small community based on the internet dedicating for gaming and learning in programming & software developing.'} />
+          <meta name='google-adsense-account' content='ca-pub-6163526907517542' />
 
           <title>Hyperion Foundation</title>
 
-          <link rel="icon" href="/hyperion-favicon.png" type="image/png" />
+          <link rel='icon' href='/hyperion-favicon.png' type='image/png' />
           <Script 
-          id="adsbygoogle-init"
-          strategy="afterInteractive"
-          crossOrigin="anonymous"
-          src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-6163526907517542" />
+          id='adsbygoogle-init'
+          strategy='afterInteractive'
+          crossOrigin='anonymous'
+          src='https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-6163526907517542' />
+
+          <Script 
+          src='https://fundingchoicesmessages.google.com/i/pub-6163526907517542?ers=1' 
+          nonce='TbOKQcNbpUaI4kAzWwmw7A' />
+          
+          <Script 
+          nonce='TbOKQcNbpUaI4kAzWwmw7A'>
+            (function() {function signalGooglefcPresent() {
+              if (!window.frames['googlefcPresent']) {
+                if (document.body) {
+                  const iframe = document.createElement('iframe'); 
+                  iframe.style = 'width: 0; height: 0; border: none; z-index: -1000; left: -1000px; top: -1000px;'; 
+                  iframe.style.display = 'none'; iframe.name = 'googlefcPresent'; document.body.appendChild(iframe);
+                  } else {
+                    setTimeout(signalGooglefcPresent, 0);
+                  }
+              }
+            }
+            signalGooglefcPresent();
+            })();</Script>
         </>
       )
     },
@@ -49,7 +69,7 @@ export default {
     banner: {
       key: 'Teamspeak',
       text: (
-        <a href="https://hyperionfoundation.statuspage.io/incidents/j05gv849vlps">⚠️ Our domain has been changed. Read more →</a>
+        <a href='https://hyperionfoundation.statuspage.io/incidents/j05gv849vlps'>⚠️ Our domain has been changed. Read more →</a>
       )
     },
     search: {
@@ -89,7 +109,7 @@ export default {
     footer: {
      text: (
         <span>
-          © 2022 - {new Date().getFullYear()} <a href="https://www.hyperfoundation.xyz">Hyperion Foundation</a> &mdash; {`v${versionNumber.version}`}
+          © 2022 - {new Date().getFullYear()} <a href='https://www.hyperfoundation.xyz'>Hyperion Foundation</a> &mdash; {`v${versionNumber.version}`}
         </span>
     )
     }
